5016c27f5278616f8ad597eb3a79c50bce5d0148
Rishikant Soni <108958955+ufoblivr@users.noreply.github.com>
2023-06-04 21:28:14 +0530

    Create 079_PowerSet.cpp

---
+++ Day 01/079_PowerSet.cpp
@@ -0,0 +1,23 @@
+//https://www.codingninjas.com/codestudio/problems/power-set_8230797?challengeSlug=striver-sde-challenge
+
+void solve(int idx, vector<int>& arr, vector<int> curr, vector<vector<int>>& ans) {
+    if(idx >= arr.size()){
+        ans.push_back(curr);
+        return;
+    }
+    
+    solve(idx+1, arr, curr, ans);
+
+    curr.push_back(arr[idx]);
+    solve(idx+1, arr, curr, ans);
+}
+
+vector<vector<int>> pwset(vector<int>arr){
+    vector<vector<int>> ans;
+    vector<int> curr;
+    int idx = 0;
+    
+    solve(idx, arr, curr, ans);
+
+    return ans;
+}
