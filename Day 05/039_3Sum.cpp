51e501b5af3150e726df967e8c9be13948beaf9a
Rishikant Soni <108958955+ufoblivr@users.noreply.github.com>
2023-06-08 22:37:20 +0530

    Create 039_3Sum.cpp

---
+++ Day 05/039_3Sum.cpp
@@ -0,0 +1,43 @@
+//https://www.codingninjas.com/codestudio/problems/3sum_8230739?challengeSlug=striver-sde-challenge&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+vector<vector<int>> findTriplets(vector<int>arr, int n, int K) {
+	vector<vector<int>> ans;
+	
+	sort(arr.begin(), arr.end());
+
+	for(int i=0; i<n; i++) {
+		int target = K - arr[i];
+		int front = i + 1;
+		int back = n - 1;
+
+		while(front < back) {
+			int sum = arr[front] + arr[back];
+
+			if(sum < target) {
+				front++;
+			}
+			else if(sum > target) {
+				back--;
+			}
+
+			else {
+				int x = arr[front];
+				int y = arr[back];
+				ans.push_back({arr[i], arr[front], arr[back]});
+
+				while(front < back && arr[front] == x) {
+					front++;
+				}
+				while(front < back && arr[back]==y) {
+					back--;
+				}
+			}
+		}
+
+		while(i+1<n && arr[i]==arr[i+1]) {
+			i++;
+		}
+	}
+	return ans;
+}
