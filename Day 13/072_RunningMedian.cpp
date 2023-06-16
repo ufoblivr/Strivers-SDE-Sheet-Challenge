0184d2bd6c576b8fc9ab6bde4adc4791316184f3
Rishikant Soni <108958955+ufoblivr@users.noreply.github.com>
2023-06-16 19:45:41 +0530

    Create 072_RunningMedian.cpp

---
+++ Day 13/072_RunningMedian.cpp
@@ -0,0 +1,33 @@
+//https://www.codingninjas.com/codestudio/problems/running-median_8230682?challengeSlug=striver-sde-challenge&leftPanelTab=0
+
+#include<bits/stdc++.h>
+class MedianFinder {
+public:
+     multiset<int> nums;
+     multiset<int>::iterator it = nums.begin();
+     bool is_even = true;
+    
+    MedianFinder() { }
+    
+    void addNum(int num) 
+    {
+        nums.insert(num);
+        is_even = !is_even;
+        if (it == nums.end() || (is_even && (*it) <= num)) it++;
+        if (!is_even && (*it) > num) it--;
+    }
+    
+    int findMedian() 
+    {
+         return (!is_even)? *it : (*it + *prev(it)) / 2;
+    }
+};
+void findMedian(int *arr, int n)
+{
+    MedianFinder* obj = new MedianFinder();
+    for(int i=0;i<n;i++)
+    {
+        obj->addNum(arr[i]);
+        cout<<obj->findMedian()<<" ";
+    }
+}
