3ca0e173a0f52b0d696f085867a4ca9f4abc064e
Rishikant Soni <108958955+ufoblivr@users.noreply.github.com>
2023-06-16 19:46:50 +0530

    Create 073_MergeKSortedArrays.cpp

---
+++ Day 13/073_MergeKSortedArrays.cpp
@@ -0,0 +1,18 @@
+//https://www.codingninjas.com/codestudio/problems/merge-k-sorted-arrays_8230770?challengeSlug=striver-sde-challenge&leftPanelTab=0
+
+#include<bits/stdc++.h>
+vector<int> mergeKSortedArrays(vector<vector<int>>&kArrays, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> minh;
+    for(int i=0; i<k ; i++){
+        for(int j=0; j< kArrays[i].size(); j++){
+            minh.push(kArrays[i][j]);
+        }
+    }
+    vector<int> res;
+    while(minh.size() > 0) {
+        res.push_back(minh.top());
+        minh.pop();
+    }  
+    return res;
+}
