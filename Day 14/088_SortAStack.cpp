fce08299330ab446a63bdfacab4a94e55bf8c44a
Rishikant Soni <108958955+ufoblivr@users.noreply.github.com>
2023-06-17 21:17:33 +0530

    Create 088_SortAStack.cpp

---
+++ Day 14/088_SortAStack.cpp
@@ -0,0 +1,26 @@
+//https://www.codingninjas.com/codestudio/problems/sort-a-stack_8230787?challengeSlug=striver-sde-challenge&leftPanelTab=0
+
+void sortedInsert(stack<int> &s, int num) {
+    if(s.empty() || (!s.empty() && s.top() <num )) {
+        s.push(num);
+        return;
+    }
+    
+    int n = s.top();
+    s.pop();
+    sortedInsert(s,num);
+    s.push(n);
+    
+}
+
+void sortStack(stack<int> &s)
+{
+    if(s.empty()){
+        return;
+    }
+    
+    int num = s.top();
+    s.pop();    
+    sortStack(s);
+    sortedInsert(s,num);
+}
